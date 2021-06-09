04bd76704f376ecdd827cd09750b7003a6d1d19b
atran <atran@rollbar.com>
2021-06-09 14:07:55 -0400

    wrap client options in payload

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,12 @@ import Rollbar from 'rollbar'
 const rollbarSingleton = new Rollbar({
   accessToken: process.env.NEXT_PUBLIC_ROLLBAR_ACCESS_TOKEN,
   environment: process.env.NEXT_PUBLIC_ROLLBAR_ENVIRONMENT,
-  client: {
-    javascript: {
-      code_version: process.env.NEXT_PUBLIC_SHA,
-    }
+  payload: {
+    client: {
+      javascript: {
+        code_version: process.env.NEXT_PUBLIC_SHA,
+      }
+    },
   },
   captureUncaught: true,
   captureUnhandledRejections: true,
