ea6a26d15340c5d4fe66dd13ff04e7b169cf6e7b
atran <atran@rollbar.com>
2021-06-09 13:35:08 -0400

    add rollbar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Link from 'next/link'
+import Rollbar from 'rollbar'
 
 const SHA = process.env.NEXT_PUBLIC_SHA ? process.env.NEXT_PUBLIC_SHA : 'nada'
 
+const rollbarSingleton = new Rollbar({
+  accessToken: process.env.NEXT_PUBLIC_ROLLBAR_ACCESS_TOKEN,
+  environment: process.env.NEXT_PUBLIC_ROLLBAR_ENVIRONMENT,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 export default function IndexPage() {
+  rollbarSingleton.info('from anthony\'s nextjs app')
   return (
     <div>
       Show me the shaaaaaa: {SHA}
