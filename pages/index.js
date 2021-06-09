b3d540d597e117f66ae6468fb51950efbc02d316
atran <atran@rollbar.com>
2021-06-09 12:33:42 -0400

    edit the git commit before showing

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 
-const sha = process.env.NEXT_PUBLIC_SHA ? process.env.NEXT_PUBLIC_SHA : 'nada'
+const SHA = process.env.NEXT_PUBLIC_SHA ? process.env.NEXT_PUBLIC_SHA : 'nada'
+
+console.log(SHA)
 
 export default function IndexPage() {
+  const shaString = 'what is ' + SHA
   return (
     <div>
-      Show me the shaaaaaa: {sha}
+      Show me the shaaaaaa: {shaString}
     </div>
   )
 }
