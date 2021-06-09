b71e5422a0010c58e3ca3abdf2b9aab16ae7d589
atran <atran@rollbar.com>
2021-06-09 13:08:57 -0400

    Remove slice and add netlify.toml

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,10 @@ import Link from 'next/link'
 
 const SHA = process.env.NEXT_PUBLIC_SHA ? process.env.NEXT_PUBLIC_SHA.slice(0, 6) : 'nada'
 
-console.log(SHA)
-
 export default function IndexPage() {
-  const shaString = 'what is ' + SHA
   return (
     <div>
-      Show me the shaaaaaa: {shaString}
+      Show me the shaaaaaa: {SHA}
     </div>
   )
 }
