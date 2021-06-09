ec5ac7bd2f1ac4986b904d609e83981da2b0982f
atran <atran@rollbar.com>
2021-06-09 12:01:37 -0400

    remove hello world

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,7 @@ const sha = process.env.NEXT_PUBLIC_SHA ? process.env.NEXT_PUBLIC_SHA : 'nada'
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <div>{sha}</div>
+      Show me the shaaaaaa: {sha}
     </div>
   )
 }
