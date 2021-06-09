0b778000cf0fb000316d26e1170086c2166c8913
atran <atran@rollbar.com>
2021-06-09 13:56:16 -0400

    add codeversion to notifier

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 import Rollbar from 'rollbar'
 
-const SHA = process.env.NEXT_PUBLIC_SHA ? process.env.NEXT_PUBLIC_SHA : 'nada'
-
 const rollbarSingleton = new Rollbar({
   accessToken: process.env.NEXT_PUBLIC_ROLLBAR_ACCESS_TOKEN,
   environment: process.env.NEXT_PUBLIC_ROLLBAR_ENVIRONMENT,
+  client: {
+    javascript: {
+      code_version: process.env.NEXT_PUBLIC_SHA,
+    }
+  },
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
@@ -14,7 +17,7 @@ export default function IndexPage() {
   rollbarSingleton.info('from anthony\'s nextjs app')
   return (
     <div>
-      Show me the shaaaaaa: {SHA}
+      index page
     </div>
   )
 }
