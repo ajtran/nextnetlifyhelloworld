6908407180b215af3c6687cd27dc22860ef85f74
atran <atran@rollbar.com>
2021-06-11 16:04:58 -0400

    use a new error

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const rollbarSingleton = new Rollbar({
 
 function throwError() {
   try {
-    const tomato = its.a.new.error.split()
+    const tomato = this.is.a.complelety.new.error
   } catch (e) {
     rollbarSingleton.error(e)
   }
