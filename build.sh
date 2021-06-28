3d8f73896709142d405807dc27ea7f05e93eb677
atran <atran@rollbar.com>
2021-06-27 22:55:07 -0700

    comment out upload to rollbar

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ echo NEXT_PUBLIC_SHA=$COMMIT_REF >> .env.local
 echo "building and exporting"
 yarn next build && yarn next export
 
-echo "upload sourcemaps to rollbar"
-npx rollbar-cli@0.1.0 upload-sourcemaps ./out \
---access-token $ROLLBAR_SERVER_ACCESS_TOKEN \
---code-version $COMMIT_REF \
---url-prefix 'https://nostalgic-leavitt-673da8.netlify.app/' \
---next
+# echo "upload sourcemaps to rollbar"
+# npx rollbar-cli@0.1.0 upload-sourcemaps ./out \
+# --access-token $ROLLBAR_SERVER_ACCESS_TOKEN \
+# --code-version $COMMIT_REF \
+# --url-prefix 'https://nostalgic-leavitt-673da8.netlify.app/' \
+# --next
